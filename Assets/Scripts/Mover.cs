52bd68ee1f4298efcec603838f01ba44a7bb9f75
thisisstreamliner <thisisstreamliner@gmail.com>
2019-09-28 15:51:06 +0300

    Unit 2.12. Introduction To Raycasting. (draw line by click on screen)

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -8,6 +8,7 @@ public class Mover : MonoBehaviour
     [SerializeField] Transform target;
 
     private NavMeshAgent _navMeshAgent;
+    private Ray lastRay;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +19,11 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+        }
         _navMeshAgent.SetDestination(target.transform.position);
     }
 }
