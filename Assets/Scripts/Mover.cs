e83d3749854bf6b1591b1771c6152d54b0de7f73
thisisstreamliner <thisisstreamliner@gmail.com>
2019-09-28 15:21:05 +0300

    Unit 2.10. Move Using Nav Mesh Agent

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    private NavMeshAgent _navMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _navMeshAgent.SetDestination(target.transform.position);
+    }
+}
