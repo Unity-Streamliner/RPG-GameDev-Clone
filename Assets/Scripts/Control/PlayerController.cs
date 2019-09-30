1ba675f7319d2a7777616c943603328a0def3abd
thisisstreamliner <thisisstreamliner@gmail.com>
2019-09-30 20:36:29 +0300

    Unit 4.26. Raycasting For Components. Unit 4.27. Implementing Action Priority.

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -2,37 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 using RPG.Movement;
+using RPG.Combat;
 
 namespace RPG.Control 
 {
     public class PlayerController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButton(0)) 
+        // Start is called before the first frame update
+        void Start()
         {
-            MoveToCursor();
+            
         }
-    }
 
-    private void MoveToCursor()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        bool hasHit = Physics.Raycast(ray, out hit);
-        if (hasHit) 
+        // Update is called once per frame
+        void Update()
         {
-            Mover mover = GetComponent<Mover>();
-            mover.MoveTo(hit.point);
+            if(InteractWithCombat()) return;
+            if (InteractWithMovement()) return;
         }
-        
+
+        private bool InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+                foreach(RaycastHit hit in hits)
+                {
+                    CombatTarget target = hit.transform.gameObject.GetComponent<CombatTarget>();
+                    if (target != null) 
+                    {
+                        if (Input.GetMouseButtonDown(0))
+                        {
+                            GetComponent<Fighter>().Attack(target);
+                        }
+                        return true;
+                    }
+                    
+                }
+                return false;
+        }
+
+        private bool InteractWithMovement() 
+        {
+            RaycastHit hit;
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
+            if (hasHit) 
+            {
+                if (Input.GetMouseButton(0)) 
+                {
+                    Mover mover = GetComponent<Mover>();
+                    mover.MoveTo(hit.point);
+                }
+                return true;  
+            } 
+            return false;
+        }
+
+        private static Ray GetMouseRay() => Camera.main.ScreenPointToRay(Input.mousePosition);
+
     }
-}
 }
