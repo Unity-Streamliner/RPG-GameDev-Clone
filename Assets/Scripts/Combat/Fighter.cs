ed1dafe4b5ea79e41cd2a4d7b16472272c44f2cd
thisisstreamliner <thisisstreamliner@gmail.com>
2019-10-01 19:59:16 +0300

    Unit 4.28. Move Within Range.

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -1,11 +1,32 @@
 using UnityEngine;
+using RPG.Movement;
 
 namespace RPG.Combat {
 
     public class Fighter : MonoBehaviour 
     {
-        public void Attack(CombatTarget target) 
+
+        [SerializeField] float weaponRange = 2f;
+
+        Transform target;
+
+        public void Update()
         {
+            
+            if (target != null)
+            {
+                bool isInRange = weaponRange >= Vector3.Distance(transform.position, target.position);
+                if (isInRange){
+                    target = null;
+                    GetComponent<Mover>().Stop();
+                } 
+                GetComponent<Mover>().MoveTo(target.position);
+            }
+        }
+
+        public void Attack(CombatTarget combatTarget) 
+        {
+            target = combatTarget.transform;
             print("Take that you short, squat peasant!");
         }
     }
--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -6,36 +6,41 @@ using UnityEngine.AI;
 namespace RPG.Movement 
 {
     public class Mover : MonoBehaviour
-{
-    [SerializeField] Transform target;
-
-    private NavMeshAgent _navMeshAgent;
-    private Animator _animator;
-
-    // Start is called before the first frame update
-    void Start()
     {
-        _navMeshAgent = GetComponent<NavMeshAgent>();
-        _animator = GetComponent<Animator>();
-    }
+        [SerializeField] Transform target;
 
-    // Update is called once per frame
-    void Update()
-    {
+        private NavMeshAgent _navMeshAgent;
+        private Animator _animator;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+            _animator = GetComponent<Animator>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            UpdateAnimator();
+        }
+
+        private void UpdateAnimator() 
+        {
+            Vector3 LocalVelocity = transform.InverseTransformDirection(_navMeshAgent.velocity);
+            _animator.SetFloat("forwardSpeed", LocalVelocity.z);
+        }
+
         
-        UpdateAnimator();
-    }
 
-    private void UpdateAnimator() 
-    {
-        Vector3 LocalVelocity = transform.InverseTransformDirection(_navMeshAgent.velocity);
-        _animator.SetFloat("forwardSpeed", LocalVelocity.z);
-    }
+        public void MoveTo(Vector3 destination) {
+            _navMeshAgent.SetDestination(destination);
+            _navMeshAgent.isStopped = false;
+        }
 
-    
-
-    public void MoveTo(Vector3 destination) {
-        _navMeshAgent.SetDestination(destination);
+        public void Stop()
+        {
+            _navMeshAgent.isStopped = true;            
+        }
     }
-}
 }
