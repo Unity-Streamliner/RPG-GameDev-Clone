24e1e6631c9f8fe8a7e20b9e7125e2ce5adc8cdf
Tiger <thisisstreamliner@gmail.com>
2019-10-12 18:27:33 +0300

    Unit 4.39. Trigger Death Animation

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -6,10 +6,23 @@ namespace RPG.Combat
     {
         [SerializeField] float health = 100f;
 
+        private bool _isDead = false;
+
         public void TakeDamage(float damage)
         {
             health = Mathf.Max(health - damage , 0);
             Debug.Log("health = " + health); 
+            if (health == 0) 
+            {
+                Die();
+            }
+        }
+
+        private void Die() 
+        {
+            if (_isDead) return;
+            _isDead = true;
+            GetComponent<Animator>().SetTrigger("die");
         }
     }
 }
