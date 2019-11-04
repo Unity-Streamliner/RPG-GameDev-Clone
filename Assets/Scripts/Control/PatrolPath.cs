f3cfc4308622ae8d08e6ccb4b03fba2108c12528
thisisstreamliner <thisisstreamliner@gmail.com>
2019-11-04 20:13:52 +0200

    Section 5.51. Visualising Waypoints

---
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control 
+{
+    public class PatrolPath : MonoBehaviour
+    {
+        [SerializeField] float waypointGizmoRadius = 0.3f;
+
+        private void OnDrawGizmos()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                Gizmos.color = Color.yellow; 
+                Gizmos.DrawSphere(transform.GetChild(i).transform.position, waypointGizmoRadius);
+            }
+        }
+    }
+}
