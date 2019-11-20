5cf929953fc4bbf8db72dfcb91a7d77ab4bba76b
thisisstreamliner <thisisstreamliner@gmail.com>
2019-11-20 20:14:53 +0200

    Section 7.71. Wait For Scene To Load

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,14 +1,25 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour 
     {
+
+        [SerializeField] int sceneToLoad = -1;
         
         private void OnTriggerEnter(Collider other) {
             print("Portal trigged");
-            if (other.tag == "Player") SceneManager.LoadScene("Sandbox");
+            if (other.tag == "Player") StartCoroutine(Transition());
+        }
+
+        private IEnumerator Transition() 
+        {
+            DontDestroyOnLoad(this.gameObject);
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+            print("scene loaded");
+            Destroy(this.gameObject);
         }
 
     }
