9217a548140f253ce548e189a377f93eca443016
thisisstreamliner <thisisstreamliner@gmail.com>
2019-11-21 12:07:00 +0200

    Section 7.72. Player Spawn Point.

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using UnityEngine.AI;
 
 namespace RPG.SceneManagement
 {
@@ -8,6 +9,7 @@ namespace RPG.SceneManagement
     {
 
         [SerializeField] int sceneToLoad = -1;
+        [SerializeField] Transform spawnPoint;
         
         private void OnTriggerEnter(Collider other) {
             print("Portal trigged");
@@ -18,9 +20,27 @@ namespace RPG.SceneManagement
         {
             DontDestroyOnLoad(this.gameObject);
             yield return SceneManager.LoadSceneAsync(sceneToLoad);
-            print("scene loaded");
+            Portal portal = GetOtherPortal();
+            UpdatePlayer(portal);
             Destroy(this.gameObject);
         }
 
+        private Portal GetOtherPortal() 
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+                return portal;
+            }
+            return null;
+        }
+        
+        private void UpdatePlayer(Portal portal) 
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(portal.spawnPoint.transform.position);
+            player.transform.rotation = portal.spawnPoint.transform.rotation;
+
+        }
     }
 }
