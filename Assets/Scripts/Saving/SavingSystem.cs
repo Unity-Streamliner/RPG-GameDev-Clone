bbd96ce3a98b2e7b96f503f05464397d70e14eea
Tiger <thisisstreamliner@gmail.com>
2020-01-08 20:04:11 +0200

    Section 9.93. Converting To Binary In C#.

--- Assets/Scripts/Saving/SavingSystem.cs
+++ Assets/Scripts/Saving/SavingSystem.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.IO;
 using System.Text;
+using System;
 
 namespace RPG.Saving
 {
@@ -12,8 +13,9 @@ namespace RPG.Saving
             print("Saving to " + path);
             using (FileStream stream = File.Open(path, FileMode.OpenOrCreate))
             {
-                byte[] bytes = Encoding.UTF8.GetBytes("¡Hola Mundo!");
-                stream.Write(bytes, 0, bytes.Length);
+                Transform playerTransform = GetPlayerTransform();
+                byte[] buffer = SerializeVector(playerTransform.position);
+                stream.Write(buffer, 0, buffer.Length);
                 stream.Close();
             }
         }
@@ -26,8 +28,37 @@ namespace RPG.Saving
             {
                 byte[] buffer = new byte[stream.Length];
                 stream.Read(buffer, 0, buffer.Length);
-                print("Readed from buffer " + Encoding.UTF8.GetString(buffer));
+                Transform playerTransform = GetPlayerTransform();
+                playerTransform.position = DesirializeVector(buffer);
             }
+        }
+
+        private Transform GetPlayerTransform()
+        {
+            return GameObject.FindGameObjectWithTag("Player").transform;
+        }
+
+        private void SetPlayerTransform(Vector3 position)
+        {
+            GameObject.FindGameObjectWithTag("Player").transform.position = position;
+        }
+
+        private byte[] SerializeVector(Vector3 vector)
+        {
+            byte[] vectorBytes = new byte[3 * 4];
+            BitConverter.GetBytes(vector.x).CopyTo(vectorBytes, 0);
+            BitConverter.GetBytes(vector.y).CopyTo(vectorBytes, 4);
+            BitConverter.GetBytes(vector.z).CopyTo(vectorBytes, 8);
+            return vectorBytes;
+        }
+
+        private Vector3 DesirializeVector(byte[] buffer)
+        {
+            Vector3 vector = Vector3.zero;
+            vector.x = BitConverter.ToSingle(buffer, 0);
+            vector.y = BitConverter.ToSingle(buffer, 4);
+            vector.z = BitConverter.ToSingle(buffer, 8);
+            return vector;
         }
 
         private string GetPathFromSaveFile(string saveFile)
