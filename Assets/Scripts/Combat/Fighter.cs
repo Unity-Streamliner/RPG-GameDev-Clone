f14d185321939c48658f122f4f90189d8d272ed8
Streamliner <thisisstreamliner@gmail.com>
2020-03-25 07:47:36 +0200

    Section 10.110. Creating Scriptable Objects (Added Sword Scriptable Weapon)

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -10,9 +10,9 @@ namespace RPG.Combat {
         [SerializeField] float weaponRange = 2f;
         [SerializeField] float timeBetweenAttacks = 1f;
         [SerializeField] float weaponDamage = 10f;
-        [SerializeField] GameObject weaponPrefab = null;
+        [SerializeField] Weapon weapon = null;
         [SerializeField] Transform handTransform = null;
-        [SerializeField] AnimatorOverrideController weaponOverride = null;
+        
 
         GameObject target;
         float timeSinceLastAttack = Mathf.Infinity;
@@ -46,8 +46,8 @@ namespace RPG.Combat {
 
         private void SpawnWeapon() 
         {
-            Instantiate(weaponPrefab, handTransform);
-            _animator.runtimeAnimatorController = weaponOverride;
+            if (weapon == null) return;
+            weapon.Spawn(handTransform, _animator);
         }
 
         public bool CanAttack(GameObject combatTarget) 
